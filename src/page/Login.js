c4e91e023ad9789e174f12a9848df1008fd93563
fool1280 <anhnguyen@Anhs-MacBook-Pro.local>
2020-06-08 18:06:53 -0500

    add login page

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
 
 export default function Login() {
     return (
         <div>
-            <h1>This is Login Page</h1>
+            <Container className="bg-light text-dark p-3 mx-auto rounded" style={{marginTop: "15%", marginBottom: "15%"}}>
+            <Form>
+                <Form.Text>
+                    <h1>Login to iJobs</h1>
+                </Form.Text>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        Your email will only be shared with employers.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+                </Form>
+            </Container>
         </div>
     )
 }
