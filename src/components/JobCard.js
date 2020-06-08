9b51abf45406448a450b6534b5890bf514e6a8aa
fool1280 <anhnguyen@Anhs-MacBook-Pro.local>
2020-06-08 18:27:16 -0500

    login page required

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
 import { Col, Row, Button, Collapse, Badge } from 'react-bootstrap'
+import { useHistory } from "react-router-dom";
 
 const badgeVariant = ["primary", "success", "info", "secondary", "danger", "secondary"]
 
 export default function JobCard(props) {
+    let history = useHistory();
     const [open, setOpen] = useState(false);
 
     const formatDate = (time) => {
         let temp = new Date(time);
         temp = temp.toDateString();
         return (temp)
+    }
+    
+    const handleClick = (id) => {
+        history.push(`/jobs/:${id}`);
     }
 
     let item = props.job;
@@ -61,11 +67,13 @@ export default function JobCard(props) {
                 </Row>
               </Col>
               <Col md={2} className="d-flex flex-column justify-content-center align-items-center border-left border-white">
-                  <h3>{item.city}</h3>
-                  <p>District {item.district}</p>
-                  <h3>Posted at:</h3>
-                  <p>{formatDate(item.time)}</p>
-                  <Button className="btn-block" variant="success">Apply!</Button>
+                    <h3>{item.city}</h3>
+                    <p>District {item.district}</p>
+                    <h3>Posted at:</h3>
+                    <p>{formatDate(item.time)}</p>
+                    <Button className="btn-block" variant="success" onClick={() => handleClick(item.id)}>
+                        Apply
+                    </Button>
               </Col>
           </Row>
         </div>
